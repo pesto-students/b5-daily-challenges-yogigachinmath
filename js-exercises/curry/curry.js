97049ecc73e3301b944457100b3972f578fd3b51
Yogishankar 1 <yogi.shankar@mountblue.io>
2021-01-20 18:52:46 +0530

    Completed the prolem

---
+++ js-exercises/curry/curry.js
@@ -0,0 +1,11 @@
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    }
+    return curried.bind(this, ...args);
+  };
+}
+export {
+  curry,
+};
